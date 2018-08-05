03b802d37fc2d9c3137c71aec6016d31fff80f4f
Corbin <brown_corbin@hotmail.com>
2018-08-05 13:21:19 -0600

    Added function onRequest. Added Favicon handling. Added /images folder.
    Broke out onRequest function from within http.createServer. Might be easier to work with.
    
    Added handling of the '/flavicon.ico' request. Should see SF logo in browser tabs.

--- hello.js
+++ hello.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env nodejs
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World, Cloue really sucks\n');
-}).listen(8080, 'localhost');
+var fs = require('fs');
+
+function onRequest(request, response){
+	//Print request to console
+	console.log("Request has been made: " + request.url);
+	
+	//If the request is for the favicon
+	if (request.url === '/favicon.ico'){
+		response.writeHead(200, {"Content-Type": "image/x-icon"})
+		fs.createReadStream("images/SF_wBackground_32x32.png").pipe(response);
+		return
+	}
+
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World, Cloue really sucks\n");
+	response.end();
+}
+
+http.createServer(onRequest).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
