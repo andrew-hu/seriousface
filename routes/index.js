4fb3c2baf0a9c2565f9c53c8a9fb51629e52c985
eclix <brown_corbin@hotmail.com>
2018-08-08 15:40:44 -0600

    Added webhook for Discord when form is submitted.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+//Probably should not include the ID and TOKEN here on a public github but oh well...
+const hook = new Discord.WebhookClient('476174292300726273','njPXKclhBKULrEVS4wlFNh-QlpgiQckf9Eem2B4bUtachjlfJxiv8tDPfW0HoNcst3X_');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
- 	res.render('index', { title: 'Serious Face' });
+ 	res.render('index.hbs', { title: 'Serious Face' });
 });
 
 // Get Apply page.
-router.get('/apply', function(req, res, next){
+router.get('/apply' || '/apply.html', function(req, res, next){
 	res.render('apply', {title: 'Apply to Serious Face'});
 });
 
@@ -21,9 +26,9 @@ router.post('/apply/submit', function(req, res, next){
 	var charactername = req.body.charactername;
 	var servername = req.body.servername;
 	var whyjoin = req.body.whyjoin;
-	console.log(charactername);
-	console.log(servername);
-	console.log(whyjoin);
+	
+	hook.send('Character Name: \t' +charactername + '\nServer Name:\t\t\t' + servername + '\nWhy Join?:\t\t\t\t' + whyjoin);
+
 	res.redirect('/thankyou');
 });
 
