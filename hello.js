8e4a0b3a1ba2ad6b62f52e2172dc3d5f6566b7d8
eclix <brown_corbin@hotmail.com>
2018-08-08 12:00:01 -0600

    Enabled Express.js. Initial Express.js commit using static index.html

--- hello.js
+++ hello.js
@@ -7,11 +7,31 @@ function onRequest(request, response){
 	console.log("Request has been made: " + request.url);
 	
 	//If the request is for the favicon
-	if (request.url === '/favicon.ico'){
-		response.writeHead(200, {"Content-Type": "image/x-icon"})
-		fs.createReadStream("images/SF_wBackground_32x32.png").pipe(response);
-		return
-	}
+	// if (request.url === '/favicon.ico'){
+	// 	response.writeHead(200, {"Content-Type": "image/x-icon"})
+	// 	fs.createReadStream("images/SF_wBackground_32x32.png").pipe(response);
+	// 	return
+	// }
+
+	switch(request.url){
+		case '/favicon.ico':
+			response.writeHead(200, {"Content-Type": "image/x-icon"})
+			fs.createReadStream("images/SF_wBackground_32x32.png").pipe(response);
+			return
+
+		case '/css/serious.css':
+			console.log("CSS REQUEST HERE");
+			response.writeHead(200, {"Content-Type": "text/css"})
+			fs.createReadStream("css/serious.css").pipe(response);	
+			return
+
+		case '/':
+			response.writeHead(200, {"Content-Type": "text/html"})
+			fs.createReadStream("index.html").pipe(response);
+			return
+
+
+	};
 
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World, Cloue really sucks\n");
